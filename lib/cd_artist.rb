93fc1bcf17e56983c8a902da43c1eb5ad78263dc
Joshua Storey & Xolani Mfuko <student@epicodus.com>
2014-08-06 10:05:02 -0700

    Create spec for artist list storage.Spec passes

--- lib/cd_artist.rb
+++ lib/cd_artist.rb
@@ -1,7 +1,14 @@
 class Artist
   attr_reader(:name)
+  @@artist_list
+
 
   def initialize(attributes)
     @name = attributes[:name]
   end
+
+  def artist_list
+    @@artist_list = []
+  end
+
 end
--- spec/cd_spec.rb
+++ spec/cd_spec.rb
@@ -3,9 +3,15 @@ require 'cd_artist'
 require 'cd_album'
 
 describe Artist do
+
   it'initializes the artist with name' do
     my_artist = Artist.new({:name => "Billy Bob Thornton"})
     my_artist.should be_an_instance_of Artist
+  end
+
+  it'initializes an empty array to store all artist names' do
+    my_artist = Artist.new({:name =>"Billy Bob Thornton"})
+    my_artist.artist_list.should eq ([])
   end
 
 end
